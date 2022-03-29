396b6f8faa258cc1d5a0cf0e93e09e8a4544fb24
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-03-29 18:21:41 +0100

    Updated scripts to work with either Arch / Debian pi Set docker installation and setup as disabled by default

--- scripts/setup_env.zsh
+++ scripts/setup_env.zsh
@@ -2,37 +2,74 @@
 
 echo -e "\n<<< Starting Environment Setup >>>\n"
 
-# update local repository
-sudo apt update
+if grep -i "debian" /etc/issue; then
+    # update local repository
+    sudo apt update
 
-# update all packages to latest versions
-sudo apt full-upgrade
+    # update all packages to latest versions
+    sudo apt full-upgrade
 
-# programs to install with apt
-programs=( neofetch tmux tree vim zsh )
+    # programs to install with apt
+    programs=( neofetch tmux tree vim zsh )
 
-for i in "${programs[@]}"
-do
-    # check if program is already installed
-    if command -v $i &> /dev/null; then
-        echo "$i exists, skipping install"
+    for i in "${programs[@]}"
+    do
+        # check if program is already installed
+        if command -v $i &> /dev/null; then
+            echo "$i exists, skipping install"
+        else
+            echo "installing $i"
+
+            # install using apt
+            sudo apt install -y "$i"
+        fi
+    done
+
+
+    # set zsh as the default shell
+    if [ "$SHELL" != "/bin/zsh" ]; then
+        echo "changing default shell to zsh"
+        chsh -s /bin/zsh $USER
     else
-        echo "installing $i"
-
-        # install using apt
-        sudo apt install -y "$i"
+        echo "Zsh already set as default shell."
     fi
-done
+elif grep -i "arch" /etc/issue; then
+    # Update system packages
+    sudo pacman -Syu --noconfirm
 
-# install zplug
-if [ -d ~/.zplug ]; then
-    echo "zplug exists, skipping install"
+    # programs to install with pacman
+    programs=( htop ncdu neofetch tmux tree vim zsh zsh-completions )
+
+    for i in "${programs[@]}"
+    do
+        # check if program is already installed
+        if command -v $i &> /dev/null; then
+            echo "$i exists, skipping install"
+        else
+            echo "installing $i"
+
+            # install using apt
+            sudo pacman -S --noconfirm "$i"
+        fi
+    done
+
+    # change default shell to Zsh
+    if [ "$SHELL" != "/usr/bin/zsh" ]
+    then
+        echo "changing default shell to zsh"
+        chsh -s /usr/bin/zsh $USER
+    else
+        echo "Zsh already set as default shell."
+    fi
 else
-    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+    echo "Distribution cannot be determined. No packages installed."
+    exit 1
 fi
 
-# set zsh as the default shell
-if [ "$SHELL" != "/bin/zsh" ]; then
-    echo "changing default shell to zsh"
-    chsh -s /bin/zsh $USER
+# install zplug
+if [ ! -d "$HOME/.zplug" ]
+then
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+else
+    echo "Zplug already installed."
 fi
